5f38feee4c027e3b5ebf756a7bd6ea89da7b6e3f
Fabio Gibson <fabiogibson@hotmail.com>
2017-04-28 17:56:09 -0300

    update refs to the original author

--- livesync/asyncserver/__init__.py
+++ livesync/asyncserver/__init__.py
@@ -1,2 +1,4 @@
+# This code was orignally written by Johan Hanssen Seferidis
+# available at https://github.com/Pithikos/python-websocket-server
 from server import WebsocketServer
 from hub import Hub
